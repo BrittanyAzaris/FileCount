98311e9765d94c3c56387994fab31da35884feff
Brittany Biggs <brittany@Brittanys-MacBook-Pro-2.local>
2019-09-02 22:27:02 +1200

    Make icons toggle when clicked

--- file-counter/src/App.js
+++ file-counter/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/App.css';
-import FileList from './components/FileList.jsx';
+// import FileList from './components/FileList.jsx';
+import Attempt2 from './components/Attempt2.jsx';
+// <FileList className='fileList'/>
 
 function App() {
   return (
     <div className='App'>
       <h1>Folders go here</h1>
-      <FileList className='fileList'/>
+      <Attempt2 />
     </div>
   );
 }
---
+++ file-counter/src/components/Attempt2.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {MDBIcon} from 'mdbreact';
+import {FaFile, FaFolder, FaFolderOpen, FaAngleRight, FaAngleDown} from 'react-icons/fa'
+
+
+export default class FileList extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://chal-locdrmwqia.now.sh/')
+    .then(response => response.json())
+    .then((jsonData) => {
+      this.setState({ data: jsonData.data, isLoaded: true})
+    })
+    .catch(console.log)
+    // call method here not in render
+
+  }
+
+  toggleFolder(item, id) {
+    console.log(item)
+
+    console.log("item id: " + id)
+    // toggle icon
+    //if open folder clicked
+    if (document.getElementById(id+'open').getAttribute('class') === 'visible icons') {
+      document.getElementById(id+'open').setAttribute('class', 'hidden icons')
+      document.getElementById(id+'closed').setAttribute('class', 'visible icons')
+    } else {
+      document.getElementById(id+'open').setAttribute('class', 'visible icons')
+      document.getElementById(id+'closed').setAttribute('class', 'hidden icons')
+    }
+    if( item.type === 'folder' ){
+      //display/hide children
+      item.children.map(child => {
+        const childId = id + '/' +child.name
+        console.log('childid: ' + childId)
+        document.getElementById(childId).setAttribute('class', 'red')
+      })
+    }
+
+  }
+
+  displayItem(item, parent) {
+    if( item.type === "folder" ) {
+      const id = parent + '/' + item.name;
+      const item_children = item.children.map( child => {
+        return this.displayItem(child, id)
+      })
+
+      return (
+        <div id={parent + '/' + item.name} className='visible' onClick={
+            (e) => {
+              // Prevent parents also being called
+              e.cancelBubble = true;
+              e.stopPropagation();
+
+              this.toggleFolder(item, id);
+            }
+          }>
+          <div className='visible icons' id={id+'open'}>
+            <FaAngleDown />
+            <FaFolderOpen />
+          </div>
+          <div className='hidden icons' id={id+'closed'}>
+            <FaAngleRight />
+            <FaFolder />
+          </div>
+          <li className='folderTitle'>{item.name}</li>
+          <div className='indent'>
+            {item_children}
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div id={parent + '/' + item.name} className='visible'>
+          <FaFile />
+          <li className='fileTitle'>{item.name + ' ' + item.size + 'MB'}</li>
+        </div>
+      )
+    }
+  }
+
+  displayFoldersNav() {
+    if( this.state.isLoaded ) {
+      return this.state.data.map( item => {
+        return this.displayItem(item, "top");
+      })
+    }
+  }
+
+  render() {
+    // console.log(Object.values(this.state.data)[0])
+
+      return (
+        <div>
+          {this.displayFoldersNav()}
+          <hr/>
+          <h3>Total Files: {this.state.totalFiles}</h3>
+          <h3>Total Filesize: {this.state.totalFileSize} MB</h3>
+        </div>
+      );
+  }
+
+}
